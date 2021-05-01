da4ca86dd879e81992b45fcf0058235ef9dc0dcb
vundelayamini <vundelayamini@gmail.com>
2021-05-01 17:53:45 +0530

    [yamini]Add: Welcome Message to Generic concepts

---
+++ GenericsDemoConcepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsDemoConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Generics Concepts");
+        }
+    }
+}
